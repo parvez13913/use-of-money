0c915e8bf3786fed64a31b77bc062f5748c8f973
parvez13913 <parvezz13913@gmail.com>
2022-02-17 06:31:05 +0600

    savings Amount and Remaining Balance complite

--- js/money.js
+++ js/money.js
@@ -1,4 +1,5 @@
 function calculateTotalCost(cost){
+    // DeclareId-------------
     const foodInput = document.getElementById('food-cost');
     const foodText = foodInput.value;
     const foodCost = parseFloat(foodText);
@@ -11,15 +12,28 @@ function calculateTotalCost(cost){
     const incomeInput = document.getElementById(cost);
     const incomeText = incomeInput.value;
     const income = parseFloat(incomeText);
+    // CalculateTotalExpensesAndBalance----------
     const totalExpenses = foodCost + rentCost + clothescost ;
+    const totalBalance = income - totalExpenses;
     document.getElementById('total-expenses').innerText = totalExpenses;
-    document.getElementById('balance').innerText = income - totalExpenses;
-
+    document.getElementById('balance').innerText = totalBalance;
+    return totalBalance;
 }
 document.getElementById('calculate-button').addEventListener('click',function(){
-    calculateTotalCost('total-income',true);
-    calculateTotalCost('total-income',false);
+    calculateTotalCost('total-income');
 });
-function savings(){
-    
+
+// savingsAmount--------
+function savingTotalAmount(save){
+    const saveInput = document.getElementById(save).value;
+    const saveAmount = parseFloat(saveInput);
+    const balance = calculateTotalCost('total-income');
+    const savingsAmount = saveAmount * balance / 100;
+    // set savings amount--------
+    document.getElementById('saving-amount').innerText = savingsAmount;
+    // set remainingBalance-------
+    document.getElementById('remaining-balance').innerText = balance - savingsAmount;
 }
+document.getElementById('save-button').addEventListener('click',function(){
+    savingTotalAmount('save-input')
+});
