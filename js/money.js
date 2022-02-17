d3c4f61194a3ed2d6b7f73cc7e1475be34fcb237
parvez13913 <parvezz13913@gmail.com>
2022-02-17 10:53:14 +0600

    Validate Complite

--- js/money.js
+++ js/money.js
@@ -1,4 +1,4 @@
-function calculateTotalCost(cost){
+function calculateTotalCost(incomeValue){
     // DeclareId-------------
     const foodInput = document.getElementById('food-cost');
     const foodText = foodInput.value;
@@ -9,31 +9,73 @@ function calculateTotalCost(cost){
     const clothesInput = document.getElementById('clothes-cost');
     const clothesText = clothesInput.value;
     const clothescost = parseFloat(clothesText);
-    const incomeInput = document.getElementById(cost);
-    const incomeText = incomeInput.value;
-    const income = parseFloat(incomeText);
-    // CalculateTotalExpensesAndBalance----------
-    const totalExpenses = foodCost + rentCost + clothescost ;
-    const totalBalance = income - totalExpenses;
-    document.getElementById('total-expenses').innerText = totalExpenses;
-    document.getElementById('balance').innerText = totalBalance;
-    return totalBalance;
+    const incomeInput = document.getElementById(incomeValue);
+        const incomeText = incomeInput.value;
+        const income = parseFloat(incomeText);
+    // errorAlert---------
+    if(isNaN(foodCost) || isNaN(rentCost) || isNaN(clothescost) || isNaN(income)){
+        document.getElementById('notify-4').style.display = 'block';
+    }
+    else if (foodCost < 0 || clothescost < 0 || rentCost < 0 || income < 0){
+        document.getElementById('notify-3').style.display = 'block';
+    }
+    else{
+        // CalculateTotalExpensesAndBalance----------
+        const totalExpenses = foodCost + rentCost + clothescost ;
+        const totalBalance = income - totalExpenses;
+        document.getElementById('total-expenses').innerText = totalExpenses;
+        document.getElementById('balance').innerText = totalBalance;
+        // FoodInputerrorAlert---------
+        document.getElementById('notify-4').style.display = 'none';
+        document.getElementById('notify-3').style.display = 'none';
+        // RentInputerrorAlert---------
+        document.getElementById('notify-4').style.display = 'none';
+    }
 }
 document.getElementById('calculate-button').addEventListener('click',function(){
     calculateTotalCost('total-income');
 });
 
 // savingsAmount--------
-function savingTotalAmount(save){
+function savingTotalAmount(save,incomeValue){
     const saveInput = document.getElementById(save).value;
     const saveAmount = parseFloat(saveInput);
-    const balance = calculateTotalCost('total-income');
-    const savingsAmount = saveAmount * balance / 100;
-    // set savings amount--------
-    document.getElementById('saving-amount').innerText = savingsAmount;
-    // set remainingBalance-------
-    document.getElementById('remaining-balance').innerText = balance - savingsAmount;
+    const foodInput = document.getElementById('food-cost');
+    const foodText = foodInput.value;
+    const foodCost = parseFloat(foodText);
+    const rentInput = document.getElementById('rent-cost');
+    const rentText = rentInput.value;
+    const rentCost = parseFloat(rentText);
+    const clothesInput = document.getElementById('clothes-cost');
+    const clothesText = clothesInput.value;
+    const clothescost = parseFloat(clothesText);
+    const incomeInput = document.getElementById(incomeValue);
+    const incomeText = incomeInput.value;
+    const income = parseFloat(incomeText);
+    const totalExpenses = foodCost + rentCost + clothescost ;
+    const balance = income - totalExpenses;
+    const savingsAmount = (saveAmount * balance) / 100;
+    // errorAlert---------
+    if(balance < savingsAmount){
+        document.getElementById('notify-1').style.display = 'block';
+    }
+    else if(saveAmount < 0){
+        document.getElementById('notify-3').style.display = 'block';
+    }
+    else if(isNaN(saveAmount)){
+        document.getElementById('notify-4').style.display = 'block';
+    }
+    else{
+        // set savings amount--------
+        document.getElementById('saving-amount').innerText = savingsAmount;
+        // set remainingBalance-------
+        document.getElementById('remaining-balance').innerText = balance - savingsAmount;
+        // errorAlert---------
+        document.getElementById('notify-1').style.display = 'none';
+        document.getElementById('notify-3').style.display = 'none';
+        document.getElementById('notify-4').style.display = 'none';
+    }
 }
 document.getElementById('save-button').addEventListener('click',function(){
-    savingTotalAmount('save-input')
+    savingTotalAmount('save-input','total-income')
 });
